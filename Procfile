9f825fd7af1f4540ff8564c98335482bb3f4333e
Leo <leo.currie@gmail.com>
2017-06-26 23:16:37 +0100

    Second commit!

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: build/install/gradle-getting-started/bin/gradle-getting-started
+web: build/install/app/bin/app
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,20 @@
 import static spark.Spark.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+
+		// Get port config of heroku on environment variable
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 8080;
+        }
+        setPort(port);
+
+		get("/", (req, res) -> "Hello, World!!");
+
     }
 }
